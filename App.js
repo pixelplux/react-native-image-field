471f2ad81783a7c628d656f9245fd27e6fb6af06
Ali <ali@ali.com>
2019-04-09 18:44:59 +0200

    Working example

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
 import { ImageAlbumComponent } from './components/image-album/image-album.component';
 
 export const App = () => (
-  <View>
-    <Text>Welcome to React Native!</Text>
-    <ImageAlbumComponent images={[]} title="Select image" />
-  </View>
+  <SafeAreaView>
+    <View>
+      <ImageAlbumComponent images={[]} title="Select image" />
+    </View>
+  </SafeAreaView>
 );
